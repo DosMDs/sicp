cb3429f27bd42b0d6a09b653092c66e602b6da99
Oleg Skakunov <dosmds@gmail.com>
2025-02-13 10:55:12 +0300

    fix

--- sicp/chapter1/03.rkt
+++ sicp/chapter1/03.rkt
@@ -3,16 +3,7 @@
 (#%require rackunit)
 
 (define (square a) (* a a))
-(define (sum a b c)
-  (+ (square
-      (if (> a
-             b)
-          a
-          b))
-     (square
-      (if (> a
-             c)
-          a
-          c))))
+(define (sum-square-two-big-numbers a b c)
+  (+ (square (if (> a b) a (if (> b c) b c))) (square (if (> c b) c (if (> a b) b a)))))
 
-(check-equal? (sum 1 2 3) 13)
+(check-equal? (sum-square-two-big-numbers 2 1 3) 13)
